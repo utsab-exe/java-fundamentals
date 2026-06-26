1f97c9e0dadfe071f797c2061db97cf5f61920f2
utsab-exe <utsabkhadka369@gmail.com>
2026-06-26 16:34:44 +0545

    okay learning my 1st data structure(Arrays)

---
+++ src/com/arrays/introToArrays.java
@@ -0,0 +1,32 @@
+package com.arrays;
+
+public class introToArrays {
+    public static void main(String[] args) {
+        //Q: Store a role number:
+        int a = 19;
+
+
+        //Q: Store a person's name.
+        String name = "Utsab Khadka";
+
+        //Q: Store 5 roll numbers:
+        int rno1 = 23;
+        int rno2 = 55;
+        int rno3 = 18;
+
+        //we can't do this for a larger no. of data, so we use arrays
+
+        //Syntax of Arrays:
+        // data_type[] variable_name = new data_type[size];
+
+        //Store 5 roll no:
+//        int[] rnos = new int[5];
+//        //or directly iterate:
+//        int[] rnos2 = {1,3,4,5,6};
+
+        //Array is a collection of value of similar data-types.
+
+        int[] ros; //declaration of array. ros is getting defined in the stack.
+        ros = new int[5]; // initialization: actually here object is being created in the memory(heap)
+    }
+}
